b9f56e73edf4fa983479d2f2ba2cd1bf99650e6a
Dave <dchuck@dal.ca>
2022-11-04 00:49:15 -0300

    installed joi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -19,6 +20,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res)=> {
+    if(!req.body.name || req.body.name.length < 3) {
+        res.status(400).send('Name is required and should be minimum 3 characters');
+        return;
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
